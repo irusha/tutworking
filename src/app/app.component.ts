6f04ae87520c8f6246d6afa8651af278c48565fc
athuk <irushaathukorala@gmail.com>
2023-05-03 00:34:06 +0530

    Thumb component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataGrabberService} from "./data-grabber.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HomeHub';
+  apiData: any;
+  tempVideo: any;
+
+  constructor(private apiService: DataGrabberService) {
+  }
+
+  ngOnInit(): void {
+    this.apiService.getData("http://localhost:5000/recom/").subscribe(res => {
+      this.apiData = res
+      this.tempVideo = this.apiData.data[2]
+    })
+  }
 }
