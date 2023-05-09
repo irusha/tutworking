352258631541c6f82bf3fef672755b8144b1de56
athuk <irushaathukorala@gmail.com>
2023-05-09 23:39:21 +0530

    Pagination updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,17 +35,20 @@ export class HomeComponent implements OnInit, AfterViewInit {
       );
 
     let secondDataSetObj: any;
-    this.apiService.getData(serverAddress + "/library/?max=8&page=" + this.pageNumber).subscribe(res => {
+    this.apiService.getData(serverAddress + "/library/?max=12&page=" + this.pageNumber).subscribe(res => {
       secondDataSetObj = res
       this.secondDataSet = secondDataSetObj.data
       this.maxPages = secondDataSetObj.pages
-      for (let i = 1; i <= secondDataSetObj.pages; i++) {
-        this.pages?.push(i)
+      if (secondDataSetObj.pages > 7) {
+        this.pages.push(...this.pagesArrayCreator(this.pageNumber, secondDataSetObj.pages))
+      } else {
+        for (let i = 1; i <= secondDataSetObj.pages; i++) {
+          this.pages?.push(i)
+        }
       }
+
       console.log(this.pages)
     })
-
-
   }
 
   ngAfterViewInit(): void {
@@ -59,6 +62,33 @@ export class HomeComponent implements OnInit, AfterViewInit {
       let recommTitle = this.recommendationTitle?.nativeElement
       recommTitle.style.display = 'none'
     }
+  }
 
+  pagesArrayCreator(page: number, maxPages: number) {
+    let pages = []
+    if (page == maxPages || page == maxPages - 1) {
+      for (let i = page - 4; i <= maxPages; i++) {
+        if (i > 0) {
+          pages.push(i)
+        }
+      }
+    }
+
+    else if (page == 1 || page == 2) {
+      for (let i = 1; i <= 5; i++) {
+        if (i <= maxPages) {
+          pages.push(i)
+        }
+      }
+    }
+
+    else {
+      for (let i = page - 2; i <= page + 2; i++) {
+        if (i <= maxPages) {
+          pages.push(i)
+        }
+      }
+    }
+    return pages
   }
 }
