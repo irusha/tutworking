19f66bb262a80a53d0990f2059d6924af56be879
athuk <irushaathukorala@gmail.com>
2023-05-16 21:28:03 +0530

    Delete function implemented

--- src/app/manage-thumbnail/manage-thumbnail.component.ts
+++ src/app/manage-thumbnail/manage-thumbnail.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {angularAddress, serverAddress} from "../../environments/environment";
+import {ActivatedRoute} from "@angular/router";
+import {DataGrabberService} from "../data-grabber.service";
 
 @Component({
   selector: 'app-manage-thumbnail',
@@ -8,5 +10,32 @@ import {angularAddress, serverAddress} from "../../environments/environment";
 })
 export class ManageThumbnailComponent {
   address: string = angularAddress;
+  requestId = ''
   @Input() videoThumbObj: any
+  @ViewChild('deleteConf') deleteConf: ElementRef | undefined
+
+  getRequestId(videoId: number) {
+    let object: any
+    let deleteConf = this.deleteConf?.nativeElement
+    deleteConf.style.display = 'block'
+
+    this.apiService.getData(serverAddress + '/delete/?video-id=' + videoId).subscribe(res => {
+      object = res
+      this.requestId = object.requestId
+      console.log(object.requestId)
+    })
+  }
+  closeDeleteDB() {
+    let deleteConf = this.deleteConf?.nativeElement
+    deleteConf.style.display = 'none'
+  }
+  deleteVideo() {
+    this.apiService.getData(serverAddress + '/delete/?request-id=' + this.requestId).subscribe(res => {
+      console.log(res)
+    })
+    this.closeDeleteDB()
+    location.reload()
+  }
+  constructor(private apiService: DataGrabberService) {
+  }
 }
